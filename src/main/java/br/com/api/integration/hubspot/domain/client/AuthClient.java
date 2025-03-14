ffe90dc23fbb7742f9570fd586001b4207b53d32
Matheus <matheuspcouto70@gmail.com>
2025-03-14 20:49:06 -0300

    Ajustes finais

--- src/main/java/br/com/api/integration/hubspot/domain/client/AuthClient.java
+++ src/main/java/br/com/api/integration/hubspot/domain/client/AuthClient.java
@@ -13,14 +13,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Tag(name = "Auth - 1° Passo", description = "API de Autorizacao")
 public interface AuthClient {
 
-    @PostMapping("/authorize")
+    @PostMapping("/authenticate")
     @Operation(summary = "Gerar URL de Autenticação do HubSpot",
             description = "Retorna a URL de autenticação do HubSpot que deve ser utilizada para gerar os tokens de acesso. " +
                     "Após receber a URL, copie-a e cole em uma nova aba do navegador, faça o login ou crie a conta na HubSpot para receber os tokens de acesso.")
     @ApiResponse(responseCode = "200", description = "Success", content = @Content)
     ResponseEntity<String> generateAuthenticationUrl();
 
-    @GetMapping("/oauth-callback")
+    @GetMapping("/callback")
     @Operation(hidden = true)
     ResponseEntity<String> handleOauthCallback(@RequestParam("code") @Valid String code);
 }
